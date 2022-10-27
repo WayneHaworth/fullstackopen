71f418cb1dfac99a12dc456a05c190f1a9b40cbf
Wayne Haworth <waynehaworth@gmail.com>
2022-10-27 10:07:12 +0100

    end 1a

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-const App = () => (
-  <div>
-    <p>Hello World</p>
-  </div>
-)
+const Hello = (props) => {
+  return(
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const name = "wayne"
+  const age = 42
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello name={name} age={age}/>
+    </div>
+  )
+}
 
 export default App
