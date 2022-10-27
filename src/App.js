5b567ee651da248928bf58d5154a856c862d2edd
Wayne Haworth <waynehaworth@gmail.com>
2022-10-27 22:25:34 +0100

    Up to Events

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
-const Hello = ({ age , scores, name }) => {
-  const bornYear = () => new Date().getFullYear() - age
-  const getAverage = (arr) => {
-    let running = 0
-    arr.forEach(score => running += score)
-    return running / arr.length
-  }
-  
-  return (
-    <div>
-      <p>Hello {name}, you are {age} years old. Your average score is: {getAverage(scores)}</p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
+import { useState } from "react";
 
 const App = () => {
-  const name = "Peter"
-  const age = 10
-  const scores = [10, 20, 30, 40, 50, 60, 70]
+  const [counter, setCounter] = useState(10);
 
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Hello name={name} age={age} scores={scores}/>
-    </div>
-  )
-}
+    <>
+      <div>{counter}</div>
+    </>
+  );
+};
 
-export default App
+export default App;
