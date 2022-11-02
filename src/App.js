e9cf9f78b40ed25bb548a8056ee53dc02ad7a683
Wayne Haworth <waynehaworth@gmail.com>
2022-11-02 09:09:16 +0000

    play with json

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import { useState } from "react";
+import axios from "axios";
+import React from "react";
 
-const Button = ({ buttonHandle }) => <button onClick={buttonHandle}>Reset value</button>;
+const client = axios.create({
+  baseURL: "https://fakestoreapi.com/products",
+});
 
+const Post = ({ title, category, image }) => {
+  return (
+    <div>
+      <img width="40" src={image} alt={title} />
+      <h1>{title}</h1>
+      <p>{category}</p>
+    </div>
+  );
+};
 
 const App = () => {
-  const [value, setValue] = useState(100);
+  const [posts, setPosts] = React.useState([]);
 
-  const increaseValue = () => {
-    setValue(0);
-  };
+  React.useEffect(() => {
+    client.get("/").then((response) => {
+      setPosts(response.data);
+    });
+  }, []);
+
+  const shortlist = posts.slice(0, 5);
 
   return (
     <div>
-      {value}
-      <Button buttonHandle={increaseValue} />
+      {shortlist.map((post) => (
+        <Post
+          key={post.id}
+          title={post.title}
+          category={post.category}
+          image={post.image}
+        />
+      ))}
     </div>
   );
 };
