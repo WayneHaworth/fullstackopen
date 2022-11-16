a33a79b1386578d003124974c4644ebdaae481b3
Wayne Haworth <waynehaworth@gmail.com>
2022-11-16 14:48:12 +0000

    Exercise 1.11

--- src/App.js
+++ src/App.js
@@ -1,43 +1,70 @@
-import axios from "axios";
-import React from "react";
+import { useState } from "react";
 
-const client = axios.create({
-  baseURL: "https://fakestoreapi.com/products",
-});
 
-const Post = ({ title, category, image }) => {
+const Button = ({text, handleClick}) => {
   return (
-    <div>
-      <img width="40" src={image} alt={title} />
-      <h1>{title}</h1>
-      <p>{category}</p>
-    </div>
-  );
-};
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  if (good || neutral || bad) {
+    return (
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={good + neutral + bad} />
+          <StatisticLine text="average" value={((good + neutral + bad) / 3).toFixed(2)} />
+          <StatisticLine text="positive" value={(good / (good + neutral + bad) * 100).toFixed(2)} />
+        </tbody>
+      </table>
+    )
+  } else return (
+    <p>No feedback given</p>
+  )
+}
+
+const StatisticLine = ({value, text}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
 
 const App = () => {
-  const [posts, setPosts] = React.useState([]);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  React.useEffect(() => {
-    client.get("/").then((response) => {
-      setPosts(response.data);
-    });
-  }, []);
+  const handleClick = (feedback) => {
+    if (feedback === "good") {
+      return setGood(good + 1);
+    } else if (feedback === "neutral") {
+      return setNeutral(neutral + 1);
+    } else if (feedback === "bad") {
+      return setBad(bad + 1);
+    }
+  }
 
-  const shortlist = posts.slice(0, 5);
+  const handleAll = () => {
+    return good + neutral + bad;
+  } 
 
   return (
     <div>
-      {shortlist.map((post) => (
-        <Post
-          key={post.id}
-          title={post.title}
-          category={post.category}
-          image={post.image}
-        />
-      ))}
+      <h1>Give feedback</h1>
+      <Button text="good" handleClick={() => handleClick("good")}/>
+      <Button text="neutral" handleClick={() => handleClick("neutral")}/>
+      <Button text="bad" handleClick={() => handleClick("bad")}/>
+
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   );
-};
+}
 
 export default App;
